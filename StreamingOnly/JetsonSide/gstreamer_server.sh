cf8632b3aa573c9274ddee8170768e4e069cc463
Matt <slylock2@mac.com>
2020-02-06 03:55:34 +0000

    changes for GRC 2019

--- StreamingOnly/JetsonSide/gstreamer_server.sh
+++ StreamingOnly/JetsonSide/gstreamer_server.sh
@@ -3,8 +3,9 @@
 #takes three arguments: camera, client IP, client port
 #e.g bash gstreamer_server.sh 1 192.168.1.34 5805
 #e.g bash gstreamer_server.sh 1 10.34.19.5 5805
+sleep 25
 
 gst-launch-1.0 -v -e v4l2src device=/dev/video$1 -v !\
-     'video/x-raw, format=(string)I420, width=(int)640, height=(int)480' !\
-     omxh264enc bitrate=600000  ! 'video/x-h264, stream-format=(string)byte-stream' !\
+     'video/x-raw, format=(string)I420, width=(int)320, height=(int)240' !\
+     omxh264enc bitrate=200000  ! 'video/x-h264, stream-format=(string)byte-stream' !\
      h264parse ! rtph264pay ! udpsink host=$2 port=$3
